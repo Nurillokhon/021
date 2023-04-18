fe807594bfb1d829657f2c7bbc2b6ada9891df53
Sabina <kamoljuraey7721@gmail.com>
2023-04-18 17:47:23 +0500

    salom sabina

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nimdir from './componets/Nimdir';
+import Sabina from './componets/Sabina'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nimdir/>
+        <Sabina/>
       </header>
     </div>
   );
